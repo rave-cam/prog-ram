243108a17ca9caf74382551ea5936c326a9d0089
Rave Camariñas <79243778+rave-cam@users.noreply.github.com>
2021-02-22 21:57:19 +0800

    Update MDAS.c

--- MDAS.c
+++ MDAS.c
@@ -12,3 +12,19 @@ printf("quotient: %d\n", quotient);
 difference = integer1 - integer2;
 printf("difference: %d\n", difference);
 return 0; }
+
+#include <stdio.h>
+int main(){
+int integer1=15, integer2=10, sum, product, quotient, difference;
+printf("Integer1: 15\n");
+printf("Integer2: 10\n");
+sum = integer1 + integer2;
+printf("sum: %d\n", sum);
+product = integer1 * integer2;
+printf("product: %d\n", product);
+quotient = integer1 / integer2;
+printf("quotient: %d\n", quotient);
+difference = integer1 - integer2;
+printf("difference: %d\n", difference);
+return 0; }
+
